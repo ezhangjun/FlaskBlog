c29aeb70c26a98b60bcde8f44565768c89191051
ezhangjun <ezhangjun@qq.com>
2017-04-07 23:18:59 +0800

    评论管理

--- app/main/views.py
+++ app/main/views.py
@@ -187,3 +187,37 @@ def post(id):
     comments = pagination.items
     return render_template('post.html', posts=[post], form=form,
                            comments=comments, pagination=pagination)
+
+@main.route('/moderate')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate():
+    page = request.args.get('page', 1, type=int)
+    pagination = Comment.query.order_by(Comment.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+        error_out=False)
+    comments = pagination.items
+    return render_template('moderate.html', comments=comments,
+            pagination=pagination, page=page)
+
+@main.route('/moderate_enable<int:id>')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate_enable(id):
+    comment = Comment.query.get_or_404(id)
+    comment.disabled=False
+    db.session.add(comment)
+    db.session.commit()
+    return  redirect(url_for('.moderate',
+                             page=request.args.get('page',1,type=int)))
+
+@main.route('/moderate_disable<int:id>')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate_disable(id):
+    comment = Comment.query.get_or_404(id)
+    comment.disabled=True
+    db.session.add(comment)
+    db.session.commit()
+    return  redirect(url_for('.moderate',
+                             page=request.args.get('page',1,type=int)))
